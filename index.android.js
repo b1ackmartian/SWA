29892649f140190488bb590e59e9fb05111a54ec
Sayo Ogunlegan <ogunlegano@gmail.com>
2017-07-31 21:03:44 -0700

    people

--- index.android.js
+++ index.android.js
@@ -9,45 +9,84 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  FlatList,
+  TouchableHighlight
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Container from './components/Container';
+import People from './components/People';
+
+const links = [
+  { title: 'People' },
+  { title: 'Films' },
+  { title: 'StarShips' },
+  { title: 'Vehicles' },
+  { title: 'Species' },
+  { title: 'Planets' }
+];
 
 export default class StarWarsApp extends Component {
+  static navigationOptions = {
+    headerTitle: <Image
+          style={{ width: 110, height: 64 }}
+          source={{
+            uri: "https://raw.githubusercontent.com/dabit3/react-native-in-action/chapter6/sw.jpg"
+          }}
+        />,
+    headerStyle: { backgroundColor: "black", height: 110 }
+  }
+
+  navigate = (link) => {
+    const { navigate } = this.props.navigation;
+    navigate(link)
+  }
+
+  renderItem = ({ item, index }) => {
+    return (
+      <TouchableHighlight
+        onPress={() => this.navigate(item.title)}
+        style={[styles.item, { borderTopWidth: index === 0 ? 1 : null }]}>
+        <Text style={styles.text}>{item.title}</Text>
+      </TouchableHighlight>
+    )
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Container>
+        <FlatList
+          data={links}
+          keyExtractor={(item) => item.title}
+          renderItem={this.renderItem}
+        />
+      </Container>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  item: {
+    padding: 20,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    borderColor: 'rgba(255,232,31, .2)',
+    borderBottomWidth: 1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+    color: '#ffe81f',
+    fontSize: 18
+  }
 });
 
-AppRegistry.registerComponent('StarWarsApp', () => StarWarsApp);
+const Navigation = StackNavigator({
+  Home: {
+    screen: StarWarsApp,
+  },
+  People: {
+    screen: People
+  }
+});
+
+AppRegistry.registerComponent('StarWarsApp', () => Navigation);
